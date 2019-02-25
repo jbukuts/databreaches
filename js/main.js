6ef2a5456b0799399f50aa7c2251c1303ec5a1cf
thebukut <jbukuts@gmail.com>
2019-02-25 16:39:01 -0500

    general styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // data time
 
+
 d3.csv("data/breaches.csv").then(function(data){
 	// log the data
 	console.log(data);
@@ -11,7 +12,16 @@ d3.csv("data/breaches.csv").then(function(data){
 
 	console.log(data.length);
 
+
+	console.log(data[0].type);
+
 	$("#breach-amount").text("There Were " + data.length + " Total Breaches");
 
 
 });
+
+
+
+function getRandom(lo, hi){
+	return Math.floor(Math.random() * hi) + lo;
+}
