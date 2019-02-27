026a14db3be1557b9e8f2ce0ffeb8e659970d6b6
thebukut <jbukuts@gmail.com>
2019-02-27 12:15:48 -0500

    tooltip works

--- js/main.js
+++ js/main.js
@@ -40,9 +40,7 @@ d3.csv("data/breaches.csv").then(function(data){
 		}
 	}
 
-	for(var i=0;i<59;i++){
-		// console.log(states_file[i].abbreviation + ": "+ states_amount[i]);
-	}
+	console.log(states_amount);
 
 	// draw the US in this svg id
 	uStates.draw("#statesvg");
@@ -54,14 +52,43 @@ d3.csv("data/breaches.csv").then(function(data){
 		$(curr).css('fill',color);
 	}
 
-	
 	// displays the total amount of breaches
 	$("#breach-amount").text("There Were " + data.length + " Total Breaches");
 	
 	// gives a random reason for one of the breaches
 	$("#reason").text('"'+data[getRandom(0,data.length-1)].type+'"');
+
+	// checks for hover and display amount of breaches
+	$(document).ready(function(){
+ 		$("path").hover(function(){
+ 			// index in array of state
+ 			var index = states_file.findIndex(x => x.abbreviation==this.id);
+    		
+    		// console.log(this.id + ": " + states_amount[index]);
+    		// bring tooltip to view
+    		d3.select("#tooltip").transition().duration(200).style("opacity", 1);  
+
+    		// change text in tooltip
+    		d3.select("#tooltip").html(tooltipHtml(this.id, states_amount[index]))  
+				.style("left", (event.pageX) + "px")     
+				.style("top", (event.pageY - 28) + "px");
+    	}, function(){
+    		// set tooltip back to 0 opacity
+    		d3.select("#tooltip").transition().duration(500).style("opacity", 0); 
+    		
+  		});
+	});
+
+
 });
 
+
+// simple fucntion to add tooltip and display breaches
+function tooltipHtml(n, d){
+		return "<h4>"+n+"</h4><table>"+
+			"<tr><td>Breaches: </td><td>"+(d)+"</td></tr>"+
+			"</table>";
+}
 
 // quick function to return a random num between hi and low
 function getRandom(lo = 0, hi){
